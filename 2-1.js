78c0f8b3071561e08b92406662c04b05241c8981
Tanzeel ur Rehman <tanzeel.dw@gmail.com>
2020-04-08 23:29:59 +0500

    Chapters 2 and 3 added

---
+++ 2-1.js
@@ -0,0 +1,16 @@
+/* 
+Problem: Write a loop that makes seven calls to console.log to output the following triangle:
+
+#
+##
+###
+####
+#####
+######
+#######
+
+*/
+
+for (let i = 1; i <= 7; ++i) {
+    console.log('#'.repeat(i));
+}
